59e0a4ff1a6aac0e66d1d5d44728e9eb3ed095bb
Tony Cebzanov <tonycpsu@gmail.com>
2024-11-30 00:12:04 -0500

    Debugging

--- bluesky-navigator.user.js
+++ bluesky-navigator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BlueSky Navigator
 // @description  Adds Vim-like navigation, read/unread post-tracking, and other features to Bluesky
-// @version      2024-11-30.01
+// @version      2024-11-30.02
 // @author       @tonycpsu
 // @namespace    https://tonyc.org/
 // @match        https://bsky.app/*
@@ -1538,5 +1538,6 @@ function loadNavigator() {
 }
 
 $(document).ready(function(e) {
+    console.log(navigator.userAgent)
     loadNavigator()
 })
